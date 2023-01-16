05614ad503498b1e9fc6ba442ed4b878b1875627
songzhipeng <1789971989@qq.com>
2023-01-16 22:20:45 +0800

    初始项目结构提交

---
+++ gulimaiil-coupon/src/main/java/com/song/gulimail/coupon/GulimaiilCouponApplication.java
@@ -0,0 +1,13 @@
+package com.song.gulimail.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimaiilCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimaiilCouponApplication.class, args);
+    }
+
+}
